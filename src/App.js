7e83ee2d159f5eda579fbd21c5d327921b4b6c88
rshm <rshm.varma@gmail.com>
2023-03-16 18:50:44 +0100

    Add question answer and timer with question list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import { MDBRow, MDBCol, MDBListGroup, MDBBtn } from "mdb-react-ui-kit";
+import {MDBRow, MDBCol, MDBListGroup, MDBBtn} from "mdb-react-ui-kit";
 import './App.css';
 import QuestionsList from './data/QuestionsList';
 import Quiz from "./components/Quiz";
-
+import Timer from "./components/Timer";
+import Start from "./components/Start";
+import {rightPanel} from "./data/data";
 function App() {
     const [selectedQuestions, setSelectedQuestions] = useState([]);
     const [name, setName] = useState(null);
     const [questionNumber, setQuestionNumber] = useState(1);
     const [timeOut, setTimeOut] = useState(false);
     const [earned, setEarned] = useState("₹ 0");
-
 
     const handleSelectedQuestions = (questions) => {
         console.log('QUESTIONS ARE-------------');
@@ -25,44 +26,84 @@ function App() {
     return (
 
         <div className="App">
-            <MDBRow>
-                <MDBCol md="9">
-                    <div className="main">
-                        <div style={{ height: "50%", position: "relative" }}>
-                            <div className="timer">
-                            </div>
-                        </div>
-                        <div style={{ height: "50%" }}>
-                            {selectedQuestions.length > 0 ? (
-                                <Quiz data={selectedQuestions}
-                                      questionNumber={questionNumber}
-                                      setQuestionNumber={setQuestionNumber}
-                                      setTimeOut={setTimeOut}/>
+            {!name ? (
+                <Start setName={setName} setTimeOut={setTimeOut}/>
+            ) : (
+                <MDBRow>
+                    <MDBCol md="9">
+                        <div className="main">
+                            {timeOut ? (
+                                <h1 className="earned">You Earned Total: {earned}</h1>
                             ) : (
-                                <QuestionsList handleSelectedQuestions={handleSelectedQuestions} />
+                                <>
+                                    <div style={{height: "50%", position: "relative"}}>
+                                        <div className="timer">
+                                            <Timer
+                                                setTimeOut={setTimeOut}
+                                                questionNumber={questionNumber}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div style={{height: "50%"}}>
+                                        {selectedQuestions.length > 0 ? (
+                                            <Quiz data={selectedQuestions}
+                                                  questionNumber={questionNumber}
+                                                  setQuestionNumber={setQuestionNumber}
+                                                  setTimeOut={setTimeOut}/>
+                                        ) : (
+                                            <QuestionsList handleSelectedQuestions={handleSelectedQuestions}/>
+                                        )}
+                                    </div>
+                                </>
                             )}
                         </div>
-                    </div>
-                </MDBCol>
-                <MDBCol md="3">
-                </MDBCol>
-            </MDBRow>
+                    </MDBCol>
+
+                    <MDBCol md="3" className="money">
+                        <MDBListGroup className="money-list">
+                            <MDBRow>
+                         <span className="mb-2">
+                         <MDBBtn
+                             style={{float: "right"}}
+                             className="mx-2"
+                             color="light"
+                             onClick={() => setTimeOut(true)}
+                         >
+                             Quit
+                          </MDBBtn>
+                        <MDBBtn
+                            style={{float: "right"}}
+                            onClick={() => {
+                                setName(null);
+                                setQuestionNumber(1);
+                                setEarned("₹ 0");
+                            }}
+                        >
+                    Exit
+                  </MDBBtn>
+                    </span>
+                        <MDBCol md="6">Name: {name}</MDBCol>
+                        <MDBCol md="6">Total Earned: {earned}</MDBCol>
+                            </MDBRow>
+                            <hr/>
+                            {rightPanel.map((item) => (
+                                <>
+                                    <li
+                                        className={
+                                            questionNumber === item.id ? "item active" : "item"
+                                        }
+                                    >
+                                        <h5 className="amount">{item.question}</h5>
+                                    </li>
+                                </>
+                            ))}
+                        </MDBListGroup>
+                    </MDBCol>
+                </MDBRow>
+            )}
         </div>
 
 
-        //
-        // <div className="App">
-        //     <header className="App-header">
-        //         <h1>Quiz App</h1>
-        //     </header>
-        //     <div className="container">
-        //         {selectedQuestions.length > 0 ? (
-        //             <Quiz data={selectedQuestions} />
-        //         ) : (
-        //             <QuestionsList handleSelectedQuestions={handleSelectedQuestions} />
-        //         )}
-        //     </div>
-        // </div>
     );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const rightPanel = [
+    {id: 1, question: "Question 1"},
+    {id: 2, question: "Question 2"},
+    {id: 3, question: "Question 3"},
+    {id: 4, question: "Question 4"},
+    {id: 5, question: "Question 5"},
+    {id: 6, question: "Question 6"},
+    {id: 7, question: "Question 7"},
+    {id: 8, question: "Question 8"},
+    {id: 9, question: "Question 9"},
+    {id: 10, question: "Question 10"},
+    {id: 11, question: "Question 11"},
+    {id: 12, question: "Question 12"},
+    {id: 13, question: "Question 13"},
+    {id: 14, question: "Question 14"},
+    {id: 15, question: "Question 15"},
+].reverse();
+
